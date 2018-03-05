cf32c6b2ba3820d1c53685668e8c1cd07b085c3b
Andy Taylor <andy@ataylor.xyz>
2018-03-05 22:55:55 +1100

    11.4% error on housing prices

--- src/ml/environments/supervised/housingprices.py
+++ src/ml/environments/supervised/housingprices.py
@@ -6,9 +6,33 @@ from . import preprocessing as pp
 
 class HousingPrices():
     def __init__(self):
-        pp.load_file("housing-prices/train.csv")
+        self.X, self.y = pp.load_file("housing-prices/train.csv")
+        self.m = len(self.y)
 
-        self.num_features = 1
+        self.trainX = self.X[:int(np.ceil(self.m * 0.6))]
+        self.crossX = self.X[int(np.floor(self.m * 0.6)):int(np.ceil(self.m * 0.8))]
+        self.testX = self.X[int(np.floor(self.m * 0.8)):]
 
-    def on_render(screen):
+        self.trainy = self.y[:int(np.ceil(self.m * 0.6))]
+        self.crossy = self.y[int(np.floor(self.m * 0.6)):int(np.ceil(self.m * 0.8))]
+        self.testy = self.y[int(np.floor(self.m * 0.8)):]
+
+        self.num_features = self.X.shape[1]
+
+    def on_render(self, screen):
         pass
+
+    def perc_error(self, predict, dataset='train'):
+        if dataset == 'train':
+            X = self.trainX
+            y = self.trainy
+        elif dataset == 'cross':
+            X = self.crossX
+            y = self.crossy
+        else:
+            X = self.testX
+            y = self.testy
+
+        predictions = predict(X)
+        perc = np.subtract(1, np.divide(predictions, y.T))
+        return np.abs(perc).mean()
--- src/states/Simulation.py
+++ src/states/Simulation.py
@@ -23,6 +23,9 @@ class Simulation(State):
         self.environment = Environment()
         self.agent = Agent(self.environment.num_features)
 
+        self.num_iters = 3000
+        self.iteration = 0
+
     def on_init(self):
         pass
 
@@ -36,7 +39,27 @@ class Simulation(State):
         pass
 
     def on_update(self, elapsed):
-        pass
+        self.agent.train(self.environment.trainX, self.environment.trainy, self.num_iters)
+
+        self.iteration += self.num_iters
+
+        print("-" * 30)
+        print("Iteration", self.iteration)
+        print("Cost:", self.agent.cost(np.insert(self.environment.trainX, 0, 1, axis=1), self.environment.trainy, len(self.environment.trainy)))
+        print("Rmsle:", self.rmsle(self.environment.testy, self.agent.predict(self.environment.testX)))
+        print("Train Perc Error:", self.environment.perc_error(self.agent.predict))
+        print("Test  Perc Error:", self.environment.perc_error(self.agent.predict, dataset='test'))
+
+        examples = np.random.randint(0, high=len(self.environment.testy), size=5)
+        predictions = self.agent.predict(self.environment.testX)
+        for i in examples:
+            print(int(predictions[i]), "->", int(self.environment.testy[i]))
+
+    def rmsle(self, y_true, y_pred):
+        print(y_true.shape)
+        print(y_pred.shape)
+        assert len(y_true) == len(y_pred)
+        return np.square(np.log(y_pred + 1) - np.log(y_true + 1)).mean() ** 0.5
 
     def on_render(self, screen):
         self.environment.on_render(screen)
